dcd79ef42885119b9166af4414205c46ba80366c
mathieuBelliveau <36576490+mathieuBelliveau@users.noreply.github.com>
2021-07-04 14:49:55 -0300

    Bank Account Project
    Completed a small object-oriented project simulating a bank account.

---
+++ BankAccount/BankAccount/BankAccount.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { 
+            get
+            {
+                decimal balance = 0;
+                foreach(Transaction trans in transactions)
+                {
+                    balance += trans.Amount;
+                }
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> transactions = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+
+            Number = accountNumberSeed++.ToString();
+        }
+
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            transactions.Add(deposit);
+        }
+
+        public void makeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Must be positive");
+            }
+            if (Balance - amount < 0)
+                throw new InvalidOperationException("Insufficient funds");
+
+            var withdrawal = new Transaction(-amount, date, note);
+            transactions.Add(withdrawal);
+        }
+
+        public string getAccountHistory()
+        {
+            var report = new StringBuilder();
+
+            report.AppendLine("Date\tAmmount\tNote");
+            foreach(var trans in transactions)
+            {
+                report.AppendLine($"{trans.Date.ToShortDateString()}\t{trans.Amount}\t{trans.Notes}");
+
+            }
+
+            return report.ToString();
+        }
+    }
+
+    
+}
---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("matt", 2000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.makeWithdrawal(20, DateTime.Now, "test");
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            Console.WriteLine(account.getAccountHistory());
+        }
+    }
+}
