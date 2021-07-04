3a41d569a0e9d1baefc6e2b6f64e9c4004b5e3b6
mathieuBelliveau <36576490+mathieuBelliveau@users.noreply.github.com>
2021-07-04 16:58:49 -0300

    Initial Web Project
    ASP project to display a series of crafts from a stored JSON file.

---
+++ CraftWebsite/CraftWebsite/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CraftWebsite.Models
+{
+    public class Product
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("maker")]
+        public string Maker { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        public int[] Ratings { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        //Equivalent to a normal method
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+
+    }
+}
---
+++ CraftWebsite/CraftWebsite/Services/JsonFileProductService.cs
@@ -0,0 +1,36 @@
+﻿using CraftWebsite.Models;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CraftWebsite.Services
+{
+    public class JsonFileProductService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "items.json"); }
+        }
+
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions 
+                    { PropertyNameCaseInsensitive = true });
+            }
+        }
+        
+    }
+}
